0296a05a608c24c4eba0c55d8529867f145e6d57
newvicx <chrisnewville1396@gmail.com>
2023-02-12 12:28:15 -0500

    correct folder name

--- hyprxa/integrations/locks/__init__.py
+++ hyprxa/integrations/locks/__init__.py
@@ -1,9 +1,17 @@
+from enum import Enum
+
 from .memcached import MemcachedLock
 from .redis import RedisLock
 
 
 
 __all__ = [
+    "Locks",
     "MemcachedLock",
     "RedisLock",
 ]
+
+
+class Locks(str, Enum):
+    REDIS = "redis",
+    MEMCACHED = "memchached"
